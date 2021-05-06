20ef4b77aafe41484c41774e2fdac93e2b791dde
Ramesh.Bheemasenarao <ramesh.bheemasenarao@thoughtfocus.com>
2021-05-07 00:48:39 +0530

    Summary of the file added

--- RulesEngine.Domain/AuditableEntity.cs
+++ RulesEngine.Domain/AuditableEntity.cs
@@ -1,7 +1,13 @@
-﻿using System;
+﻿//-----------------------------------------------------------------------
+// <copyright file="AutoRoutingFunction.cs" company="SBR">
+// Copyright (c) SBR. All rights reserved.
+// </copyright>
+// <summary> Rules Engine </summary>
+//-----------------------------------------------------------------------
 
 namespace RulesEngine.Domain
 {
+    using System;
     public class AuditableEntity
     {
         public string CreatedBy { get; set; }
